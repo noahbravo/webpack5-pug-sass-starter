de0f56807a61f7c305102f2aa3d5437aa341977e
Elena Bravo <hey@elenabravo.com>
2022-01-02 03:17:25 +0100

    fix import

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,8 @@ import 'normalize.css/normalize.css'
 import '../sass/style.sass'
 import { onDocumentReady, importAll } from './functions.js'
 
-// import all media from ./src/assets
-importAll(
-  require.context(
-    '../assets/images',
-    false,
-    /\.(png|jpe?g|svg|gif|mov|mp4|ico|xml|webmanifest)$/
-  )
-)
+// import all images from ./src/assets
+importAll(require.context('../assets/images', false, /\.(png|jpe?g|svg|gif)$/))
 
 onDocumentReady(function () {
   console.log('hello friend.')
