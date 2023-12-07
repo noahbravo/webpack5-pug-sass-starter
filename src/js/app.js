0f9658734bca5edefef6558534b76cf2d50211da
Elena Bravo <elenabravo@Elenas-MacBook-Pro.local>
2023-12-07 01:01:47 +0100

    update pug plugin and webpack config

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,4 @@
-import 'normalize.css/normalize.css'
-import '../sass/style.sass'
 import { onDocumentReady, importAll } from './functions.js'
-
-// import all media from public
-importAll(
-  require.context(
-    '../../public',
-    true,
-    /\.(png|svg|jpg|jpe?g|gif|mov|mp4|ico|webmanifest|xml)$/
-  )
-)
 
 onDocumentReady(function () {
   console.log('hello friend.')
